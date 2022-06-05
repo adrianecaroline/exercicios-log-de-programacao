81ef1fed488fa7ee2b5e3a4f521e39ecf678395d
Driel <97626768+adrianecaroline@users.noreply.github.com>
2022-06-05 15:24:45 -0300

    Update exercicio2.js

--- Lista 2/exercicio2.js
+++ Lista 2/exercicio2.js
@@ -1,15 +1,3 @@
-/* 2)      Construa uma função para mostrar o pagamento de comissão de vendedores de peças, 
-levando-se em consideração que sua comissão será de 5% do total da venda e que você tem os seguintes dados:
-
-a)       Identificação do vendedor //string
-
-b)      Código da peça //number
-
-c)       Preço unitário da peça //number 4 reais
-
-d)      Quantidade vendida //number  2 peças  quantidade vendida * preço = 8 
-
-*/
 
 function payment (){
 
